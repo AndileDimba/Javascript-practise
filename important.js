fb7b6c35351999310b9ce137835f5ba2b8178838
Andile Dimba <mlamuliforex@gmail.com>
2021-05-20 19:14:55 +0200

    saving my work

--- important.js
+++ important.js
@@ -60,18 +60,17 @@ function dupArr(arr) {
 // 4. Return the new string we made.
 
 function reverse(str) {
-  let result = str.split(' ');
+  let result = str.split(" ");
   for (let i = 0; i < result.length; i++) {
     if (i % 2 == 1) {
-      let char = result[i].split('').reverse().join('');
+      let char = result[i].split("").reverse().join("");
       result[i] = char;
     }
   }
-  return result.join(' ');
+  return result.join(" ");
 }
 
 // console.log(reverse('one two three four'));
-
 
 //                              Fizz Buzz
 
@@ -81,44 +80,44 @@ function reverse(str) {
 // Step 4: if above condition fails, and i is divisible by 5, print “Buzz”
 // Step 5: if all conditions fails, print i itself.
 
-function fizzbuzz (num) {
-  for (let i=1; i<=num; i++) {
-      if (i%3 === 0 && i%5 === 0)
-          console.log ("FizzBuzz");
-      else if (i%3 === 0)
-          console.log ("Fizz");
-      else if (i%5 === 0)
-          console.log ("Buzz");
-      else
-          console.log (i);
-  }
-}
-
-// fizzbuzz (16);
-
-
-//                           Replace word with another one
-
-
-
-function replaceStr(str) {
-
-// 0. store the word first
-// 1. Split the string into an array so that we can go through each word
-// 2. iterate through each word
-//  - check if the string matches the word
-//  - replace it if it matches
-// 3. join the strings back with spaces
-
-  let word = 'film';
-  let result = str.split(' ');
-
-  for (let i = 0; i < result.length; i++) {
-    if (result[i] === word){
-      result[i] = 'movie';
+function fizzbuzz(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
     }
   }
   return result.join(' ');
 }
 
-console.log(replaceStr('This film is the best movie in the world!'));
+
+console.log(fizzbuzz(16));
+
+//                           Replace word with another one
+
+function replaceStr(str) {
+  // 0. store the word first
+  // 1. Split the string into an array so that we can go through each word
+  // 2. iterate through each word
+  //  - check if the string matches the word
+  //  - replace it if it matches
+  // 3. join the strings back with spaces
+
+  let word = "film";
+  let result = str.split(" ");
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === word) {
+      result[i] = "movie";
+    }
+  }
+  return result.join(" ");
+}
+
+// console.log(replaceStr('This film is the best movie in the world!'));

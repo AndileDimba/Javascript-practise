c53bd158c4b327863a43120cfb30d8df28599e50
Andile Dimba <mlamuliforex@gmail.com>
2021-05-27 13:24:48 +0200

    saving work

--- unimportant.js
+++ unimportant.js
@@ -11,4 +11,23 @@ function dupArr(arr1, arr2) {
   return newArr.filter(checkNum);
 }
 
-console.log(dupArr([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+// console.log(dupArr([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+
+//                                 remove duplicates from the array and other arguments
+
+
+function destroyer(arr) {
+  let valsToRemove = Object.values(arguments).slice(1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(item => item !== null);
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
